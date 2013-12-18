9b7cad69877b6712e4399bc8e6dd0bc98be96b34
Alex Rothberg <agrothberg@gmail.com>
2013-12-17 19:56:40 -0500

    - fix text for python3

--- pandas/tests/test_format.py
+++ pandas/tests/test_format.py
@@ -2210,12 +2210,12 @@ class TestDatetimeIndexFormat(tm.TestCase):
 
 class TestDatetimeIndexUnicode(tm.TestCase):
     def test_dates(self):
-        text = unicode(pd.to_datetime([datetime(2013,1,1), datetime(2014,1,1)]))
+        text = str(pd.to_datetime([datetime(2013,1,1), datetime(2014,1,1)]))
         self.assertTrue("[2013-01-01," in text)
         self.assertTrue(", 2014-01-01]" in text)
 
     def test_mixed(self):
-        text = unicode(pd.to_datetime([datetime(2013,1,1), datetime(2014,1,1,12), datetime(2014,1,1)]))
+        text = str(pd.to_datetime([datetime(2013,1,1), datetime(2014,1,1,12), datetime(2014,1,1)]))
         self.assertTrue("[2013-01-01 00:00:00," in text)
         self.assertTrue(", 2014-01-01 00:00:00]" in text)
 
